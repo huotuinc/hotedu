c7ca24cb7417d4637d72d630cb8629d81d22fdf1
luffyj <luffy.ja@gmail.com>
2015-06-17 01:59:53 +0800

    to English name

--- service/src/main/java/com/huotu/hotedu/repository/ManagerRepository.java
+++ service/src/main/java/com/huotu/hotedu/repository/ManagerRepository.java
@@ -1,7 +1,11 @@
 package com.huotu.hotedu.repository;
 
 import com.huotu.hotedu.entity.Manager;
+import org.luffy.lib.libspring.data.ClassicsRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,6 +14,10 @@ import org.springframework.stereotype.Repository;
  * @author luffy luffy.ja at gmail.com
  */
 @Repository
-public interface ManagerRepository extends JpaRepository<Manager,Long>{
+public interface ManagerRepository extends JpaRepository<Manager,Long>,JpaSpecificationExecutor<Manager>,ClassicsRepository<Manager> {
+
+    @Modifying
+    @Query("update Manager u set u.managerField=?1")
+    int updateManagerField(String managerField);
 
 }
