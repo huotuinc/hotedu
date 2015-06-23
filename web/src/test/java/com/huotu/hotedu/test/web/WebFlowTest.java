31143d5bb440b521cc6155c4f6682a2c047198d7
shiliting <861819089@qq.com>
2015-06-23 14:16:04 +0800

    readmev1.0

--- web/src/test/java/com/huotu/hotedu/test/web/WebFlowTest.java
+++ web/src/test/java/com/huotu/hotedu/test/web/WebFlowTest.java
@@ -1,94 +1,94 @@
-package com.huotu.hotedu.test.web;
-
-import com.huotu.hotedu.entity.Member;
-import com.huotu.hotedu.repository.LoginRepository;
-import com.huotu.hotedu.repository.MemberRepository;
-import com.huotu.hotedu.service.LoginService;
-import com.huotu.hotedu.test.TestWebConfig;
-import libspringtest.SpringWebTest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpSession;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-/**
- * Created by luffy on 2015/6/10.
- *
- * @author luffy luffy.ja at gmail.com
- */
-@ActiveProfiles("test")
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestWebConfig.class)
-@WebAppConfiguration
-public class WebFlowTest extends SpringWebTest {
-
-    protected MockHttpSession loginAs(String userName, String password) throws Exception {
-        MockHttpSession session = (MockHttpSession) this.mockMvc.perform(get("/"))
-                .andReturn().getRequest().getSession(true);
-        //bad password
-        session = (MockHttpSession) this.mockMvc.perform(post("/login").session(session)
-                .param("username", userName).param("password", password))
-                .andDo(print())
-                .andReturn().getRequest().getSession();
-
-//         CsrfToken token = new HttpSessionCsrfTokenRepository().loadToken(request);
-        saveAuthedSession(session);
-        return session;
-    }
-
-    @Autowired
-    private LoginService loginService;
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private LoginRepository loginRepository;
-
-    @Test
-    public void index() throws Exception {
-        mockMvc.perform(
-                get("/")
-        )
-                .andDo(print());
-    }
-
-    @Test
-    public void sayMyname() throws Exception {
-        mockMvc.perform(
-                get("/sayMyname")
-        )
-                .andExpect(status().isFound()) // forward to /login
-        ;
-
-        checkMemeber("memberdemo");
-
-        MockHttpSession session = loginAs("memberdemo", "memberdemo");
-
-        mockMvc.perform(
-                get("/sayMyname")
-                        .session(session)
-        )
-                .andExpect(status().isOk())
-                .andExpect(content().string("memberdemo"))
-        ;
-
-    }
-
-    private void checkMemeber(String name) {
-        try {
-            loginService.loadUserByUsername(name);
-        } catch (UsernameNotFoundException ex) {
-            Member member = new Member();
-            member.setLoginName(name);
-            loginService.newLogin(member, name);
-        }
-    }
-}
+//package com.huotu.hotedu.test.web;
+//
+//import com.huotu.hotedu.entity.Member;
+//import com.huotu.hotedu.repository.LoginRepository;
+//import com.huotu.hotedu.repository.MemberRepository;
+//import com.huotu.hotedu.service.LoginService;
+//import com.huotu.hotedu.test.TestWebConfig;
+//import libspringtest.SpringWebTest;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mock.web.MockHttpSession;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//
+///**
+// * Created by luffy on 2015/6/10.
+// *
+// * @author luffy luffy.ja at gmail.com
+// */
+//@ActiveProfiles("test")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = TestWebConfig.class)
+//@WebAppConfiguration
+//public class WebFlowTest extends SpringWebTest {
+//
+//    protected MockHttpSession loginAs(String userName, String password) throws Exception {
+//        MockHttpSession session = (MockHttpSession) this.mockMvc.perform(get("/"))
+//                .andReturn().getRequest().getSession(true);
+//        //bad password
+//        session = (MockHttpSession) this.mockMvc.perform(post("/login").session(session)
+//                .param("username", userName).param("password", password))
+//                .andDo(print())
+//                .andReturn().getRequest().getSession();
+//
+////         CsrfToken token = new HttpSessionCsrfTokenRepository().loadToken(request);
+//        saveAuthedSession(session);
+//        return session;
+//    }
+//
+//    @Autowired
+//    private LoginService loginService;
+//    @Autowired
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    private LoginRepository loginRepository;
+//
+//    @Test
+//    public void index() throws Exception {
+//        mockMvc.perform(
+//                get("/")
+//        )
+//                .andDo(print());
+//    }
+//
+//    @Test
+//    public void sayMyname() throws Exception {
+//        mockMvc.perform(
+//                get("/sayMyname")
+//        )
+//                .andExpect(status().isFound()) // forward to /login
+//        ;
+//
+//        checkMemeber("memberdemo");
+//
+//        MockHttpSession session = loginAs("memberdemo", "memberdemo");
+//
+//        mockMvc.perform(
+//                get("/sayMyname")
+//                        .session(session)
+//        )
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("memberdemo"))
+//        ;
+//
+//    }
+//
+//    private void checkMemeber(String name) {
+//        try {
+//            loginService.loadUserByUsername(name);
+//        } catch (UsernameNotFoundException ex) {
+//            Member member = new Member();
+//            member.setLoginName(name);
+//            loginService.newLogin(member, name);
+//        }
+//    }
+//}
