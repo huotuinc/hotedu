75f25e4a8b7d2153bd020f47dbafc4edf8eeefe4
shiliting <861819089@qq.com>
2015-06-26 09:40:45 +0800

    upload ExamGuide

---
+++ service/src/main/java/com/huotu/hotedu/entity/ExamGuide.java
@@ -0,0 +1,84 @@
+package com.huotu.hotedu.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by shiliting on 2015/6/25.
+ *
+ * @author shiliting
+ */
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
+//@Inheritance(strategy=InheritanceType.JOINED)  //有待解答
+public  class ExamGuide implements Serializable {
+
+    private static final long serialVersionUID = -349012453592429794L;//有待解答
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column(length = 5000)
+    private String content;
+    @Column
+    private boolean top=true;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUploadDate;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isTop() {
+        return top;
+    }
+
+    public void setTop(boolean top) {
+        this.top = top;
+    }
+
+    public Date getLastUploadDate() {
+        return lastUploadDate;
+    }
+
+    public void setLastUploadDate(Date lastUploadDate) {
+        this.lastUploadDate = lastUploadDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ExamGuide{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", top=" + top +
+                ", lastUploadDate=" + lastUploadDate +
+                '}';
+    }
+}
