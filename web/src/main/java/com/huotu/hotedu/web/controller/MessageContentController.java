b904a29140e162c52f0be28a234d391ea0692c9d
shiliting <861819089@qq.com>
2015-07-07 11:30:46 +0800

    modify messagecontent

--- web/src/main/java/com/huotu/hotedu/web/controller/MessageContentController.java
+++ web/src/main/java/com/huotu/hotedu/web/controller/MessageContentController.java
@@ -25,8 +25,8 @@ public class MessageContentController {
     private MessageContentService messageContentService;
     public static final int PAGE_SIZE=10;//每张页面的记录数
     //后台单击考试指南链接显示的消息
-    @RequestMapping("/backend/load/messagecontents")
-    public String loadMessageContents(Model model){
+    @RequestMapping("/backend/load/messagecontent")
+    public String loadMessageContent(Model model){
         Page<MessageContent> pages=messageContentService.loadMessageContent(0, PAGE_SIZE);
         long sumElement=pages.getTotalElements();
         model.addAttribute("allMessageContentList",pages);
@@ -34,12 +34,12 @@ public class MessageContentController {
         model.addAttribute("n",0);
         model.addAttribute("keywords","");
         model.addAttribute("sumElement",sumElement);
-        return "/backend/messagecontents";
+        return "/backend/messagecontent";
     }
 
     //后台单机搜索按钮显示的考试指南消息
-    @RequestMapping("/backend/search/messagecontents")
-    public String searchMessageContents(String keywords,Model model){
+    @RequestMapping("/backend/search/messagecontent")
+    public String searchMessageContent(String keywords,Model model){
         Page<MessageContent> pages=messageContentService.searchMessageContent(0, PAGE_SIZE, keywords);
         long sumElement=pages.getTotalElements();
         model.addAttribute("allMessageContentList",pages);
@@ -47,12 +47,12 @@ public class MessageContentController {
         model.addAttribute("n",0);
         model.addAttribute("keywords",keywords);
         model.addAttribute("sumElement",sumElement);
-        return "/backend/messagecontents";
+        return "/backend/messagecontent";
     }
 
     //后台单击考试指南的分页
-    @RequestMapping("/backend/page/messagecontents")
-    public String pageMessageContents(int n,int sumpage,String keywords,Model model){
+    @RequestMapping("/backend/page/messagecontent")
+    public String pageMessageContent(int n,int sumpage,String keywords,Model model){
         //如果已经到分页的第一页了，将页数设置为0
         if (n < 0){
             n++;
@@ -65,12 +65,12 @@ public class MessageContentController {
         model.addAttribute("n",n);
         model.addAttribute("keywords",keywords);
         model.addAttribute("sumElement",pages.getTotalElements());
-        return "/backend/messagecontents";
+        return "/backend/messagecontent";
     }
 
     //后台单击删除按钮返回的信息
-    @RequestMapping("/backend/del/messagecontents")
-    public String DelMessageContents(int n,int sumpage,String keywords,Long id,Long sumElement,Model model){
+    @RequestMapping("/backend/del/messagecontent")
+    public String DelMessageContent(int n,int sumpage,String keywords,Long id,Long sumElement,Model model){
         messageContentService.delMessageContent(id);
         if((sumElement-1)%PAGE_SIZE==0){
             if(n>0&&n+1==sumpage){n--;}
@@ -84,20 +84,20 @@ public class MessageContentController {
         model.addAttribute("n",n);
         model.addAttribute("keywords",keywords);
         model.addAttribute("sumElement",sumElement);
-        return "/backend/messagecontents";
+        return "/backend/messagecontent";
     }
 
 
 
 
     //后台单击新建按钮
-    @RequestMapping("/backend/add/messagecontents")
-    public String AddMessageContents(Model model){
+    @RequestMapping("/backend/add/messagecontent")
+    public String AddMessageContent(Model model){
         return "/backend/newmessagecontent";
     }
     //后台单机修改按钮
-    @RequestMapping("/backend/modify/messagecontents")
-    public String ModifyMessageContents(Long id, Model model){
+    @RequestMapping("/backend/modify/messagecontent")
+    public String ModifyMessageContent(Long id, Model model){
         MessageContent messageContent=messageContentService.findOneById(id);
         model.addAttribute("messagecontent",messageContent);
         return "/backend/modifymessagecontent";
@@ -105,27 +105,27 @@ public class MessageContentController {
 
 
     //后台单击添加保存按钮
-    @RequestMapping("/backend/addsave/messagecontents")
-    public String AddSaveMessageContents(String title,String content,String top,Model model){
+    @RequestMapping("/backend/addsave/messagecontent")
+    public String AddSaveMessageContent(String title,String content,String top,Model model){
         MessageContent messageContent=new MessageContent();
         messageContent.setTitle(title);
         messageContent.setContent(content);
         messageContent.setLastUploadDate(new Date());
         messageContent.setTop("1".equals(top)? true:false);
         messageContentService.addMessageContent(messageContent);
-        return "redirect:/backend/load/messagecontents";
+        return "redirect:/backend/load/messagecontent";
     }
 
 
     //后台单击修改保存按钮
-    @RequestMapping("/backend/modifysave/messagecontents")
-    public String ModifySaveMessageContents(Long id,String title,String content,Boolean top,Model model){
+    @RequestMapping("/backend/modifysave/messagecontent")
+    public String ModifySaveMessageContent(Long id,String title,String content,Boolean top,Model model){
         MessageContent messageContent=messageContentService.findOneById(id);
         messageContent.setTitle(title);
         messageContent.setContent(content);
         messageContent.setTop(top);
         messageContent.setLastUploadDate(new Date());
         messageContentService.modify(messageContent);
-        return "redirect:/backend/load/messagecontents";
+        return "redirect:/backend/load/messagecontent";
     }
 }
