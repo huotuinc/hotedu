2475199da75666bf5815020a2196045259791b16
JiaShubing <1214369231@qq.com>
2015-07-07 11:05:39 +0800

    更新js编码

--- web/src/main/webapp/backend/js/jquery.confirm.js
+++ web/src/main/webapp/backend/js/jquery.confirm.js
@@ -96,6 +96,15 @@ $(document).ready(function () {
         $.MsgBox.Confirm("温馨提示", "执行删除后将无法恢复，确定继续吗？", function () {
             alert("你居然真的删除了..."); });
     });
+            //静态页面弹出添加时后应有
+            //$("#link-add").bind("click", function () {
+            //    $.MsgBox.Alert("消息", "添加成功！");
+            //});
+//              回调函数可以直接写方法function(){}
+            $(".link-delete").bind("click", function () {
+                $.MsgBox.Confirm("温馨提示", "执行删除后将无法恢复，确定继续吗？", function () {
+                    alert("你居然真的删除了..."); });
+            });
 //            function test() {
 //                alert("你点击了确定,进行了修改");
 //            }
