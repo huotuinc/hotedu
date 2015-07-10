cf53e8a31b4f8b752cf0160da1df3562e09fedc0
chenkuanmin <chenkuanmin>
2015-07-10 16:22:25 +0800

    test still success ignoring test

--- web/src/main/java/com/huotu/hotedu/web/controller/BannersController.java
+++ web/src/main/java/com/huotu/hotedu/web/controller/BannersController.java
@@ -1,7 +1,15 @@
 package com.huotu.hotedu.web.controller;
 
+import com.huotu.hotedu.entity.Banners;
+import com.huotu.hotedu.service.BannersService;
+import com.huotu.hotedu.service.ExamGuideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 /**
  * Created by shiliting on 2015/6/10.
@@ -10,16 +18,111 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class BannersController {
+    @Autowired
+    private BannersService BannersService;
+    public static final int PAGE_SIZE=10;//每张页面的记录数
     //后台显示所有Banner图
     @RequestMapping("/backend/loadBanners")
     public String loadBannersController() {
         return "/backend/banners";
     }
 
-
-    //后台显示检索过之后的Banner图
-    @RequestMapping("/backend/searchBanners")
+    //后台显示检索过之后的Banners图
+    @RequestMapping("/backend/searchBanners1")
     public String searchBannersController() {
-        return "";
+        return "/backend/banners";
+    }
+
+    //后台单机搜索按钮显示的Banners消息
+    @RequestMapping("/backend/searchBanners2")
+    public String searchBanners(String keywords,Model model){
+        Page<Banners> pages=BannersService.searchBanners(0, PAGE_SIZE, keywords);
+        long sumElement=pages.getTotalElements();
+        model.addAttribute("allbannersList",pages);
+        model.addAttribute("sumpage",sumElement/pages.getSize()+(sumElement%pages.getSize()>0? 1:0));
+        model.addAttribute("n",0);
+        model.addAttribute("keywords",keywords);
+        model.addAttribute("sumElement",sumElement);
+        return "/backend/banners";
+    }
+
+    //后台单击Banners的分页
+    @RequestMapping("/backend/pageBanners")
+    public String pageBanners(int n,int sumpage,String keywords,Model model){
+        //如果已经到分页的第一页了，将页数设置为0
+        if (n < 0){
+            n++;
+        }else if(n + 1 > sumpage){//如果超过分页的最后一页了，将页数设置为最后一页
+            n--;
+        }
+        Page<Banners> pages = BannersService.searchBanners(n, PAGE_SIZE, keywords);
+        model.addAttribute("allbannersList",pages);
+        model.addAttribute("sumpage",sumpage);
+        model.addAttribute("n",n);
+        model.addAttribute("keywords",keywords);
+        model.addAttribute("sumElement",pages.getTotalElements());
+        return "/backend/banners";
+    }
+
+    //后台单击删除按钮返回的信息
+    @RequestMapping("/backend/delBanners")
+    public String delBanners(int n,int sumpage,String keywords,Long id,Long sumElement,Model model){
+        BannersService.delBanners(id);
+        if((sumElement-1)%PAGE_SIZE==0){
+            if(n>0&&n+1==sumpage){n--;}
+            sumpage--;
+
+        }
+        sumElement--;
+        Page<Banners> pages = BannersService.searchBanners(n, PAGE_SIZE, keywords);
+        model.addAttribute("sumpage",sumpage);
+        model.addAttribute("allbannersList",pages);
+        model.addAttribute("n",n);
+        model.addAttribute("keywords",keywords);
+        model.addAttribute("sumElement",sumElement);
+        return "/backend/banners";
+    }
+
+
+    //后台单击新建按钮
+    @RequestMapping("/backend/addbanners")
+    public String addBanners(Model model){
+        return "/backend/newbanners";
+    }
+
+    //后台单机修改按钮
+    @RequestMapping("/backend/modifyBanners")
+    public String ModifyBanners(Long id, Model model){
+        Banners banners=BannersService.findOneById(id);
+        model.addAttribute("banners",banners);
+        return "/backend/modifybanners";
+    }
+
+
+    //后台单击添加保存按钮
+    @RequestMapping("/backend/addSaveBanners")
+    public String addSaveBanners(String title,String content,String top,Model model){
+        Banners banners=new Banners();
+        banners.setTitle(title);
+        banners.setContent(content);
+        banners.setLastUploadDate(new Date());
+        banners.setTop("1".equals(top) ? true : false);
+        BannersService.addBanners(banners);
+        return "redirect:/backend/loadBanners";
+    }
+
+
+    //后台单击修改保存按钮
+    @RequestMapping("/backend/modifySaveBanners")
+    public String modifySaveBanners(Long id,String title,String content,Boolean top,Model model){
+        Banners banners=BannersService.findOneById(id);
+        banners.setTitle(title);
+        banners.setContent(content);
+        banners.setTop(top);
+        banners.setLastUploadDate(new Date());
+        BannersService.modify(banners);
+        return "redirect:/backend/loadBanners";
     }
 }
+
+
