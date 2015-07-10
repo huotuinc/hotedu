8a222cb88c3ffccbc88bb50843908e51ce97e597
chenkuanmin <chenkuanmin>
2015-07-10 16:31:27 +0800

    test still success ignoring test

---
+++ service/src/main/java/com/huotu/hotedu/entity/Banners.java
@@ -0,0 +1,76 @@
+package com.huotu.hotedu.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by shiliting on 2015/6/25.
+ *
+ * @author shiliting
+ */
+@Entity
+//@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
+//@Inheritance(strategy=InheritanceType.JOINED)  //有待解答 已在邮件中答复 :D
+public  class Banners implements Serializable {
+
+    private static final long serialVersionUID = -349012453592429794L;//有待解答...Serializable标准而已
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column(length = 1000)
+    private String content;
+    @Column
+    private boolean top=true;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUploadDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isTop() {return top;}
+
+    public void setTop(boolean top) {this.top = top;}
+
+    public Date getLastUploadDate() {
+        return lastUploadDate;
+    }
+
+    public void setLastUploadDate(Date lastUploadDate) {
+        this.lastUploadDate = lastUploadDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Banners{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", top=" + top +
+                ", lastUploadDate=" + lastUploadDate +
+                '}';
+    }
+}
