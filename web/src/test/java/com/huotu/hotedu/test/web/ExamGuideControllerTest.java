74d62c0aee8fd7d5422bd167d79fa36dee47a0c8
HZCJ <luffy.ja@gmail.com>
2015-07-21 12:03:55 +0800

    安全控制ok了

--- web/src/test/java/com/huotu/hotedu/test/web/ExamGuideControllerTest.java
+++ web/src/test/java/com/huotu/hotedu/test/web/ExamGuideControllerTest.java
@@ -1,66 +1,61 @@
 package com.huotu.hotedu.test.web;
 
+import com.huotu.hotedu.WebTestBase;
+import com.huotu.hotedu.entity.Editor;
+import com.huotu.hotedu.entity.ExamGuide;
+import com.huotu.hotedu.entity.Manager;
 import com.huotu.hotedu.entity.Member;
+import com.huotu.hotedu.repository.ExamGuideRepository;
 import com.huotu.hotedu.repository.LoginRepository;
 import com.huotu.hotedu.repository.MemberRepository;
 import com.huotu.hotedu.repository.QaRepository;
 import com.huotu.hotedu.service.LoginService;
 import com.huotu.hotedu.test.TestWebConfig;
+import com.huotu.hotedu.web.controller.ExamGuideController;
+import junit.framework.Assert;
 import libspringtest.SpringWebTest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by luffy on 2015/6/10.
  *
  * @author luffy luffy.ja at gmail.com
  */
-@ActiveProfiles("test")
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestWebConfig.class)
-@WebAppConfiguration
-public class ExamGuideControllerTest extends SpringWebTest {
+public class ExamGuideControllerTest extends WebTestBase {
 
-    protected MockHttpSession loginAs(String userName, String password) throws Exception {
-        MockHttpSession session = (MockHttpSession) this.mockMvc.perform(get("/"))
-                .andReturn().getRequest().getSession(true);
-        session = (MockHttpSession) this.mockMvc.perform(post("/login").session(session)
-                .param("username", userName).param("password", password))
-                .andDo(print())
-                .andReturn().getRequest().getSession();
-        saveAuthedSession(session);
-        return session;
-    }
+    private static final Log log = LogFactory.getLog(ExamGuideControllerTest.class);
 
     @Autowired
     private LoginService loginService;
     @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private LoginRepository loginRepository;
-    @Autowired
-    private QaRepository qaRepository;
+    private ExamGuideRepository examGuideRepository;
 
     // /backend/loadExamGuide 非登录状态无法访问
+    //
     // /backend/loadExamGuide 最多展示 n 个数据 n 自定义
+    //
     // /backend/delExamGuide  删除&查询
-    // ?id=1 要删的记录的id
-    // &n=0 删除完成以后显示的页索引   （3）
-    // &keywords= 删除以后 显示指定关键字搜索页面
-    // &sumpage=4 总共能分的页数    4页 31条数据   30
-    // &sumElement=39
-
     // 删除
     // id 要删除的ID
     // 查询
@@ -68,38 +63,124 @@ public class ExamGuideControllerTest extends SpringWebTest {
     // &n=0 删除完成以后显示的页索引
     // &paging=10 每页10条
 
+    // /backend/searchExamGuide
+    // 查询
+    // &keywords= 删除以后 显示标题包含指定关键字搜索页面
+    // &n=0 删除完成以后显示的页索引
+    // &paging=10 每页10条
+
+    // 修改的操作 需要ROLE_EDITOR 权限
+    // Editor 是可以修改的
+    // 但是Member 是不可以的
+
     @Test
+    @Rollback
     public void examGuideTest() throws Exception {
-        mockMvc.perform(
-                get("/backend/loadExamGuide")
-        )
-                .andDo(print())
-//                .andExpect(status().isForbidden())
-        ;
-    }
 
-    @Test
-    public void index() throws Exception {
-        mockMvc.perform(
-                get("/backend/pageLink?n=0;keywords='';sumpage=1")
-        ).andDo(print());
-    }
-    @Test
-    public void login() throws  Exception{
+        //准备测试环境
+        String password = UUID.randomUUID().toString();
 
+        String memberUsername = UUID.randomUUID().toString();
+        String editorUsername = UUID.randomUUID().toString();
 
-/*        mockMvc.perform(
-                get("/backend/search/examGuide")
-        ).andDo(print())
-        ;*/
-    }
-    private void checkMemeber(String name) {
-        try {
-            loginService.loadUserByUsername(name);
-        } catch (UsernameNotFoundException ex) {
-            Member member = new Member();
-            member.setLoginName(name);
-            loginService.newLogin(member, name);
+        Member member = new Member();
+        member.setLoginName(memberUsername);
+
+        Editor editor = new Editor();
+        editor.setLoginName(editorUsername);
+
+        loginService.newLogin(member, password);
+        loginService.newLogin(editor, password);
+
+        //准备需要测试的ExamGuide
+        // 10+随机10
+        String complexKeyword = UUID.randomUUID().toString();
+        // TODO 删除目前包含这个奇葩关键字的ExamGuide
+
+        Random random = new Random();
+
+        int count = 20 + random.nextInt(20);
+
+        ArrayList<ExamGuide> containsExamGuides = new ArrayList<>();
+
+        while (count-- > 0) {
+            ExamGuide examGuide = new ExamGuide();
+            examGuide.setLastUploadDate(new Date());
+            examGuide.setContent("123");
+            examGuide.setIsTop(random.nextBoolean());
+            boolean containKeyword = random.nextBoolean();
+            if (containKeyword) {
+                examGuide.setTitle("测试t" + complexKeyword + System.currentTimeMillis());
+            } else {
+                examGuide.setTitle("测试t" + System.currentTimeMillis());
+            }
+
+            examGuide = examGuideRepository.save(examGuide);
+
+            if (containKeyword)
+                containsExamGuides.add(examGuide);
         }
+        //准备测试环境END
+
+        mockMvc.perform(
+                get("/backend/searchExamGuide")
+        )
+                .andExpect(status().isFound());
+
+        int totalCount = (int) examGuideRepository.count();
+        int defaultPageSize = ExamGuideController.PAGE_SIZE;
+        int pages = totalCount/defaultPageSize+ (totalCount%defaultPageSize==0?0:1);
+
+        mockMvc.perform(
+                get("/backend/searchExamGuide")
+                        .session(loginAs(memberUsername, password))
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("allGuideList", new BaseMatcher<Object>() {
+                    @Override
+                    public boolean matches(Object o) {
+                        // 这个是一个Page类型的 第二长度必须有..
+                        if (o == null || !(o instanceof Page)) {
+                            log.error("非Page类型");
+                            return false;
+                        }
+                        Page page = (Page) o;
+                        return page.getContent().size() == ExamGuideController.PAGE_SIZE;
+                    }
+
+                    @Override
+                    public void describeTo(Description description) {
+
+                    }
+                }))
+                .andExpect(model().attribute("sumpage",pages))
+        ;
+
+        //
+        ArrayList<ExamGuide> found = new ArrayList<>();
+        int currentIndex = 0;
+
+        while(found.size()<containsExamGuides.size()){
+
+            Map<String,Object> model = mockMvc.perform(
+                    get("/backend/searchExamGuide")
+                    .param("keywords",complexKeyword)
+                    .param("n",""+currentIndex)
+                    .param("paging","1")
+            )
+                    .andExpect(status().isOk())
+                    .andReturn().getModelAndView().getModel();
+
+            Page<ExamGuide> allGuideList = (Page<ExamGuide>) model.get("allGuideList");
+            Assert.assertFalse("",allGuideList.getContent().isEmpty());
+            Assert.assertTrue("",containsExamGuides.contains(allGuideList.getContent().get(0)));
+            //
+
+            currentIndex = ((Number)(model.get("n"))).intValue()+1;
+
+            found.add(allGuideList.getContent().get(0));
+        }
+
+
     }
 }
