6ddca026fa305c0b3204a05664c1afbb657c793a
HZCJ <luffy.ja@gmail.com>
2015-07-21 10:11:18 +0800

    安全控制ok了

---
+++ web/src/test/java/com/huotu/hotedu/WebTestBase.java
@@ -0,0 +1,41 @@
+package com.huotu.hotedu;
+
+import com.huotu.hotedu.test.TestWebConfig;
+import com.huotu.hotedu.web.config.SecurityConfig;
+import libspringtest.SpringWebTest;
+import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+/**
+ * web方向测试基类
+ * @author CJ
+ */
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestWebConfig.class)
+@WebAppConfiguration
+@Transactional
+public class WebTestBase extends SpringWebTest{
+
+    protected MockHttpSession loginAs(String userName, String password) throws Exception {
+        MockHttpSession session = (MockHttpSession) this.mockMvc.perform(get("/"))
+                .andReturn().getRequest().getSession(true);
+        session = (MockHttpSession) this.mockMvc.perform(post(SecurityConfig.LoginURI).session(session)
+                .param("username", userName).param("password", password))
+//                .andDo(print())
+                .andReturn().getRequest().getSession();
+
+        saveAuthedSession(session);
+        return session;
+    }
+
+}
--- web/src/test/java/com/huotu/hotedu/test/web/WebFlowTest.java
+++ web/src/test/java/com/huotu/hotedu/test/web/WebFlowTest.java
@@ -1,55 +1,70 @@
 package com.huotu.hotedu.test.web;
 
+import com.huotu.hotedu.WebTestBase;
+import com.huotu.hotedu.entity.Manager;
 import com.huotu.hotedu.entity.Member;
 import com.huotu.hotedu.repository.ExamGuideRepository;
 import com.huotu.hotedu.service.LoginService;
-import com.huotu.hotedu.test.TestWebConfig;
-import libspringtest.SpringWebTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by luffy on 2015/6/10.
  *
  * @author luffy luffy.ja at gmail.com
  */
-@ActiveProfiles("test")
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestWebConfig.class)
-@WebAppConfiguration
-public class WebFlowTest extends SpringWebTest {
+public class WebFlowTest extends WebTestBase {
 
-    protected MockHttpSession loginAs(String userName, String password) throws Exception {
-        MockHttpSession session = (MockHttpSession) this.mockMvc.perform(get("/"))
-                .andReturn().getRequest().getSession(true);
-        session = (MockHttpSession) this.mockMvc.perform(post("/login").session(session)
-                .param("username", userName).param("password", password))
-                .andDo(print())
-                .andReturn().getRequest().getSession();
 
-        saveAuthedSession(session);
-        return session;
-    }
 
     @Autowired
     private LoginService loginService;
     @Autowired
     private ExamGuideRepository examGuideRepository;
+    @Autowired
+    private Environment env;
 
 
     @Test
-    public void login() throws  Exception{
+    @Rollback
+    public void login() throws Exception {
+
+        String username = UUID.randomUUID().toString();
+        String password = UUID.randomUUID().toString();
+
+        Manager manager = new Manager();
+        manager.setLoginName(username);
+
+        manager = loginService.newLogin(manager,password);
+
+        mockMvc.perform(
+//                get("/backend/test/open")
+                get("/backend/loadExamGuide")
+        )
+                .andExpect(status().isFound());//尚未登录是302 已登录则是403
+
+
+        MockHttpSession session = loginAs(username,password);
+
+
+        mockMvc.perform(
+                get("/backend/loadExamGuide")
+                .session(session)
+        )
+                .andExpect(status().isOk());
+
+
 //        ExamGuide exam=new ExamGuide();
 //        exam.setTitle("dd");
 //        exam.setContent("asdfe");
@@ -62,6 +77,7 @@ public class WebFlowTest extends SpringWebTest {
 //        ;
 
     }
+
     private void checkMemeber(String name) {
         try {
             loginService.loadUserByUsername(name);
