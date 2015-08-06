edaea30fd5dbfe9a136df9c2bc39b67a29a79b47
cwb <815498030@qq.com>
2015-08-06 14:25:40 +0800

    注解

--- service/src/main/java/com/huotu/hotedu/service/CertificateService.java
+++ service/src/main/java/com/huotu/hotedu/service/CertificateService.java
@@ -3,10 +3,12 @@ package com.huotu.hotedu.service;
 import com.huotu.hotedu.entity.Certificate;
 import com.huotu.hotedu.repository.CertificateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * Created by Administrator on 2015/8/6.
+ * Created by cwb on 2015/8/6.
  */
+@Service
 public class CertificateService {
 
     @Autowired
