175086087c17abe5e5cac75b20eaaee61a7641c4
shiliting <861819089@qq.com>
2015-08-13 10:29:30 +0800

    update agent picture

--- web/src/main/java/com/huotu/hotedu/web/advice/TestAdvice.java
+++ web/src/main/java/com/huotu/hotedu/web/advice/TestAdvice.java
@@ -1,7 +1,6 @@
 package com.huotu.hotedu.web.advice;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * ControllerAdvice, Advice to Controller.
@@ -13,7 +12,7 @@ public class TestAdvice {
     /**
      * 所有异常统一处理
      */
-    @ExceptionHandler(Throwable.class)
+//    @ExceptionHandler(Throwable.class)
     public String allThrowable(Throwable ex){
         //model.addAttribute("message","系统故障");
         return "redirect:/error";
--- web/src/main/java/com/huotu/hotedu/web/controller/ClassTeamController.java
+++ web/src/main/java/com/huotu/hotedu/web/controller/ClassTeamController.java
@@ -39,83 +39,6 @@ public class ClassTeamController {
      */
     public static final int PAGE_SIZE=10;
 
-//    @RequestMapping("/backend/loadClassTeam")
-//    public String loadClassTeam(Model model) {
-//        Page<ClassTeam> pages = classTeamService.loadClassTeam(0, PAGE_SIZE);
-//        long sumElement = pages.getTotalElements();
-//        model.addAttribute("allclassteamList", pages);
-//        model.addAttribute("sumpage", (sumElement + pages.getSize() - 1) / pages.getSize());
-//        model.addAttribute("n", 0);
-//        model.addAttribute("keywords", "");
-//        model.addAttribute("sumElement", sumElement);
-//        return "/backend/classteam";
-//    }
-//    /**
-//     * 搜索符合条件的classteam信息
-//     *
-//     * @param keywords 搜索关键字 可以为空
-//     * @param model    返回客户端参数集
-//     * @return examguide.html
-//     */
-//    @RequestMapping("/backend/searchClassTeam")
-//    public String searchClassTeam(@RequestParam(required = false)Integer pageNo,
-//                                  @RequestParam(required = false)Integer pageSize,
-//                                  @RequestParam(required = false) String keywords, Model model) {
-//        if(pageNo==null){
-//            pageNo=0;
-//        }
-//        if(pageSize==null) {
-//            pageSize = PAGE_SIZE;
-//        }
-//        Page<ClassTeam> pages;
-//        if (keywords == null) {
-//            pages = classTeamService.loadClassTeam(pageNo, pageSize);
-//        }
-//        else {
-//            pages = classTeamService.searchClassTeam(pageNo, pageSize, keywords);
-//        }
-//        long sumElement = pages.getTotalElements();
-//        model.addAttribute("allclassteamList", pages);
-//        model.addAttribute("sumpage", (sumElement + pages.getSize() - 1) / pages.getSize());
-//        model.addAttribute("pageNo", pageNo);
-//        model.addAttribute("keywords", keywords);
-//        model.addAttribute("sumElement", sumElement);
-//        return "/backend/classteam";
-//    }
-//
-//
-//    /**
-//     * classteam.html页面点击修改后跳转
-//     *
-//     * @param id    需要修改的id
-//     * @param model 返回客户端集
-//     * @return modifyclassteam.html
-//     */
-//    @PreAuthorize("hasRole('EDITOR')")
-//    @RequestMapping("/backend/modifyClassTeam")
-//    public String modifyClassTeam(Long id, Model model) {
-//        ClassTeam classTeam = classTeamService.findOneById(id);
-//        model.addAttribute("classteam",classTeam);
-//        return "/backend/modifyclassteam";
-//    }
-//
-//    /**
-//     * modifyclassteam.html页面点击保存修改后跳转
-//     *
-//     * @param id      修改后的id
-//     * @param exam    考试信息
-//     * @return 重定向到：/backend/loadClassTeam
-//     */
-//    @PreAuthorize("hasRole('EDITOR')")
-//    @RequestMapping("/backend/modifySaveClassTeam")
-//    public String modifySaveClassTeam(Long id, Exam exam) {
-//        ClassTeam classteam = classTeamService.findOneById(id);
-//        classteam.setExam(exam);
-//        classTeamService.modify(classteam);
-//        return "redirect:/backend/searchClassTeam";
-//    }
-
-
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping("/backend/lookAgentClass")
     public String searchClassTeam(@RequestParam(required = false)Integer pageNo,
@@ -138,7 +61,9 @@ public class ClassTeamController {
             totalRecords = pages.getTotalElements();
         }
 
-        agent.setPictureUri(staticResourceService.getResource(agent.getPictureUri()).toURL().toString());
+        if(agent.getPictureUri()!=null){
+            agent.setPictureUri(staticResourceService.getResource(agent.getPictureUri()).toURL().toString());
+        }
         model.addAttribute("totalMembers",memberService.searchMembers(agent,0,PAGE_SIZE).getTotalElements());
         model.addAttribute("agent",agent);
         model.addAttribute("allAgentClassList", pages);
