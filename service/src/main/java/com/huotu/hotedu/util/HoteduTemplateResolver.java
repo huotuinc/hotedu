4b4468b73d4d83b7980dfb7f8873dd07437b1515
cwb <815498030@qq.com>
2015-08-27 10:02:23 +0800

    自定义模板逻辑修改

--- service/src/main/java/com/huotu/hotedu/util/HoteduTemplateResolver.java
+++ service/src/main/java/com/huotu/hotedu/util/HoteduTemplateResolver.java
@@ -28,32 +28,37 @@ public class HoteduTemplateResolver extends TemplateResolver {
 
         final String templateName = templateProcessingParameters.getTemplateName();
         StringBuilder newTemplateName = new StringBuilder();
-        //检查访问方式是否为移动端
-        SpringWebContext context =  (SpringWebContext)templateProcessingParameters.getContext();
-        HttpServletRequest request = context.getHttpServletRequest();
-        HttpSession session = request.getSession();
-        //检查是否已经记录访问方式（移动端或pc端）
-        boolean isFromMobile = false;
-        if(null==session.getAttribute("ua")){
-            try{
-                //获取ua，用来判断是否为移动端访问
-                String userAgent = request.getHeader("USER-AGENT").toLowerCase();
-                if(null == userAgent){
-                    userAgent = "";
+        if(!templateName.endsWith("_mobile")) {
+            //检查访问方式是否为移动端
+            SpringWebContext context = (SpringWebContext) templateProcessingParameters.getContext();
+            HttpServletRequest request = context.getHttpServletRequest();
+            HttpSession session = request.getSession();
+            //检查是否已经记录访问方式（移动端或pc端）
+            boolean isFromMobile = false;
+            if (null == session.getAttribute("ua")) {
+                try {
+                    //获取ua，用来判断是否为移动端访问
+                    String userAgent = request.getHeader("USER-AGENT").toLowerCase();
+                    if (null == userAgent) {
+                        userAgent = "";
+                    }
+                    isFromMobile = CheckMobile.check(userAgent);
+                    //判断是否为移动端访问
+                    if (isFromMobile) {
+                        session.setAttribute("ua", "mobile");
+                    } else {
+                        session.setAttribute("ua", "pc");
+                    }
+                } catch (Exception e) {
                 }
-                isFromMobile= CheckMobile.check(userAgent);
-                //判断是否为移动端访问
-                if(isFromMobile){
-                    session.setAttribute("ua","mobile");
-                } else {
-                    session.setAttribute("ua","pc");
-                }
-            }catch(Exception e){}
-        }else{
-            isFromMobile=session.getAttribute("ua").equals("mobile");
-        }
-        if(isFromMobile) {
-            newTemplateName.append(templateName).append("_mobile");
+            } else {
+                isFromMobile = session.getAttribute("ua").equals("mobile");
+            }
+            if (isFromMobile) {
+                newTemplateName.append(templateName).append("_mobile");
+            } else {
+                newTemplateName.append(templateName);
+            }
         }else {
             newTemplateName.append(templateName);
         }
