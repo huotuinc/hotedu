4cac74dbaae5f4255f14a786792ff20ebccb6f3b
cwb <815498030@qq.com>
2015-10-28 21:58:43 +0800

    浮动广告

--- web/src/main/java/com/huotu/hotedu/web/controller/admin/NoticeController.java
+++ web/src/main/java/com/huotu/hotedu/web/controller/admin/NoticeController.java
@@ -1,12 +1,16 @@
 package com.huotu.hotedu.web.controller.admin;
 
 import com.huotu.hotedu.entity.Notice;
+import com.huotu.hotedu.entity.Result;
 import com.huotu.hotedu.repository.NoticeRepository;
+import com.huotu.hotedu.web.model.NoticeModel;
 import com.huotu.hotedu.web.service.StaticResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,6 +32,10 @@ public class NoticeController {
     @Autowired
     StaticResourceService staticResourceService;
 
+    /**
+     * 显示公告列表页面
+     * @return
+     */
     @RequestMapping(value = "/backend/notices",method = RequestMethod.GET)
     public ModelAndView showNoticeList() {
         List<Notice> noticeList = noticeRepository.findAll();
@@ -37,6 +45,11 @@ public class NoticeController {
         return  modelAndView;
     }
 
+    /**
+     * 根据ID加载公告修改页面
+     * @param noticeId
+     * @return
+     */
     @RequestMapping(value = "/backend/notices/{noticeId}",method = RequestMethod.GET)
     public ModelAndView showNoticeDetail(@PathVariable long noticeId) {
         ModelAndView modelAndView = new ModelAndView();
@@ -46,6 +59,14 @@ public class NoticeController {
         return modelAndView;
     }
 
+    /**
+     * 修改公告图片和链接地址
+     * @param pic
+     * @param noticeId
+     * @param linkUrl
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(value = "/backend/notices/{noticeId}",method = RequestMethod.POST)
     @Transactional
     public ModelAndView modifyNotice(MultipartFile pic,@PathVariable long noticeId,String linkUrl) throws Exception {
@@ -66,6 +87,18 @@ public class NoticeController {
         notice.setLinkUrl(linkUrl);
         noticeRepository.save(notice);
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/backend/changeNoticeStatus",method = RequestMethod.POST,consumes="application/json")
+    @ResponseBody
+    @Transactional
+    public Result changeNoticeStatus(@RequestBody NoticeModel noticeModel) {
+        Notice notice = noticeRepository.findOne(Long.parseLong(noticeModel.getNoticeId()));
+        notice.setEnabled("1".equals(noticeModel.getEnabled())?true:false);
+        noticeRepository.save(notice);
+        Result result = new Result();
+        result.setStatus(1);
+        return  result;
     }
 
 }
---
+++ web/src/main/java/com/huotu/hotedu/web/model/NoticeModel.java
@@ -0,0 +1,27 @@
+package com.huotu.hotedu.web.model;
+
+/**
+ * Created by WenbinChen on 2015/10/28 20:34.
+ */
+public class NoticeModel {
+
+    private String noticeId;
+
+    private String enabled;
+
+    public String getNoticeId() {
+        return noticeId;
+    }
+
+    public void setNoticeId(String noticeId) {
+        this.noticeId = noticeId;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+}
